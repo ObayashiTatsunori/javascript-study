2cf681ea4732f3e64ef1f3470079d58aa654c2db
Obayashi <4210.tatu@gmail.com>
2018-11-12 22:00:23 +0900

    7.4.1 PHPとAjaxを比較してみる

---
+++ hello.php
@@ -0,0 +1,11 @@
+<form method="POST" action="hello.php">
+  <label for="name">名前：</label>
+  <input type="text" name="name" size="15" />
+  <input type="submit" name="submit" value="送信" />
+</form>
+<?php
+if($_REQUEST['submit']!==null){
+  sleep(3);
+  print('こんにちは、'.htmlspecialchars($_POST['name'],ENT_QUOTES | ENT_HTML5,'UTF-8').'さん！');
+}
+?>
