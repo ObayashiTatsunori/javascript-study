c53ed5d9fbcbbf015612f436a1e42e9ce2b1135e
Obayashi <4210.tatu@gmail.com>
2018-11-19 22:22:07 +0900

    サーバーへの送信をGET→POSTに変更

--- scripts/hello_ajax.js
+++ scripts/hello_ajax.js
@@ -2,18 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('btn').addEventListener('click', function() {
         var result = document.getElementById('result');
         var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4) { //通信が完了した時
-                if (xhr.status === 200) { //通信が成功した時
-                    result.textContent = xhr.responseText;
-                } else { //通信が失敗した時
-                    result.textContent = 'サーバーエラーが発生しました。';
-                }
-            } else { //通信が完了する前
-                result.textContent = '通信中・・・'
-            }
-        };
+        xhr.addEventListener('loadstart', function() {
+            result.textContent = '通信中・・・';
+        }, false);
+        xhr.addEventListener('load', function() {
+            result.textContent = xhr.responseText;
+        }, false);
+        xhr.addEventListener('error', function() {
+            result.textContent　 = 'サーバーエラーが発生しました'
+        }, false)
+
+
         //サーバーとの非同期処理を開始
+        xhr.open('POST', 'hello_ajax.php', true);
+        xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+        xhr.send('name=' + encodeURLComponent(document.getElementById('name').value));
+
         xhr.open('GET', 'hello_ajax.php?name=' + encodeURIComponent(document.getElementById('name').value), true);
         xhr.send(null);
     }, false);
